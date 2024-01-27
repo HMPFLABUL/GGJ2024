4157b10c7f17be4f2cc353de465c6fc30465305f
HMPFLABUL <hmpflabul@gmail.com>
2024-01-27 14:31:21 +0100

    lil juice

--- Assets/_Game/Scripts/Misc/NpcIdle.cs
+++ Assets/_Game/Scripts/Misc/NpcIdle.cs
@@ -8,14 +8,16 @@ public class NpcIdle : MonoBehaviour
     Sequence boop;
     Vector3 top;
     Vector3 bottom;
-    [SerializeField] float boopSpeed = 2f;
+    [SerializeField] float boopSpeed = .6f;
+    [SerializeField] float idleDelay = 0f;
     bool started = false;
 
     private void Awake()
     {
         top = transform.localScale;
         bottom = new Vector3(top.x, .9f * top.y, top.z);
-        SetupBoop();
+        started = true;
+        Invoke("SetupBoop",idleDelay);
     }
 
     private void SetupBoop()
@@ -33,10 +35,9 @@ public class NpcIdle : MonoBehaviour
         boop.Append(transform.DOLocalRotate(transform.localEulerAngles + new Vector3(0, -60, 0), boopSpeed*2));
         boop.Append(transform.DOScale(top, .5f));
         boop.Append(transform.DOLocalRotate(transform.localEulerAngles + new Vector3(0, 0, 0), boopSpeed));
-        boop.Append(transform.DOScale(top, 3f));
-        
+        boop.Append(transform.DOScale(top, 3f));      
         boop.SetLoops(-1);
-        started = true;
+        
     }
 
     private void Update()
